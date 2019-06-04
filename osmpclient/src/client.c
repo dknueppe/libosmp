8bf617fe56b70265d388dc8469278e8bf7b93eff
Daniel Knüppe <daniel_k@posteo.net>
2019-06-04 10:07:16 +0200

    fixed last effed up merge

--- osmpclient/src/client.c
+++ osmpclient/src/client.c
@@ -26,74 +26,64 @@ int main(int argc, char *argv[])
     char recv[OSMP_MAX_PAYLOAD_LENGTH] = "";
     OSMP_Datatype foobar = OSMP_typeof(bar[0]);
     OSMP_Request myrequest1, myrequest2;
-
+    
     /* print out all arguments */
     printf("argv[] = ");
-    for (int i = 0; i < argc; i++)
-        printf("%s ", argv[i]);
-    printf("\n");
-    fflush(NULL);
+    for(int i = 0; i < argc; i++)
+        printf("%s ",argv[i]);
+    printf("\n");fflush(NULL);
 
     /* small nonexhaustive test of blocking functions */
-    if ((status = OSMP_Init(&argc, &argv) != OSMP_SUCCESS))
+    if((status = OSMP_Init(&argc, &argv) != OSMP_SUCCESS))
         printf("Error %d\n", error);
     error++;
-    
-    if ((status = OSMP_Size(&size) != OSMP_SUCCESS))
+    if((status = OSMP_Size(&size) != OSMP_SUCCESS))
         printf("Error %d\n", error);
     error++;
-    
-    if ((status = OSMP_Rank(&rank) != OSMP_SUCCESS))
+    if((status = OSMP_Rank(&rank) != OSMP_SUCCESS))
         printf("Error %d\n", error);
     error++;
-    
-    if ((status = OSMP_Send(bar, sizeof(bar), OSMP_typeof(bar[0]), rank) != OSMP_SUCCESS))
+    if((status = OSMP_Send(bar, sizeof(bar), OSMP_typeof(bar[0]), rank) != OSMP_SUCCESS))
         printf("Error %d\n", error);
     error++;
-    
-    if ((status = OSMP_Recv(recv, 13, foobar, &source, &len) != OSMP_SUCCESS))
+    if((status = OSMP_Recv(recv, 13, foobar, &source, &len) != OSMP_SUCCESS))
         printf("Error %d\n", error);
     error++;
     printf("message from blocking is:\t\"%s\"\n", recv);
 
     /* small nonexhaustive test of nonblocking functions */
     char irecv[OSMP_MAX_PAYLOAD_LENGTH] = "";
-    if ((status = OSMP_CreateRequest(&myrequest1)) != OSMP_SUCCESS)
+    if((status = OSMP_CreateRequest(&myrequest1)) != OSMP_SUCCESS)
         printf("Error %d\n", error);
     error++;
-    
-    if ((status = OSMP_CreateRequest(&myrequest2)) != OSMP_SUCCESS)
-        printf("Error %d\n", error);
-    error++;
-
-    if ((status = OSMP_Isend(bar, sizeof(bar), OSMP_typeof(bar[0]), rank, myrequest1)) != OSMP_SUCCESS)
-        printf("Error %d\n", error);
-    error++;
-    
-    if ((status = OSMP_Irecv(irecv, 13, OSMP_typeof(*bar), &sender, &len, myrequest2)) != OSMP_SUCCESS)
+    if((status = OSMP_CreateRequest(&myrequest2)) != OSMP_SUCCESS)
         printf("Error %d\n", error);
     error++;
 
-    if ((status = OSMP_Wait(myrequest1)) != OSMP_SUCCESS)
+    if((status = OSMP_Isend(bar, sizeof(bar), OSMP_typeof(bar[0]), rank, myrequest1)) != OSMP_SUCCESS)
         printf("Error %d\n", error);
     error++;
-    
-    if ((status = OSMP_Wait(myrequest2)) != OSMP_SUCCESS)
-        printf("Error %d\n", error);
-    error++;
-
-    if ((status = OSMP_RemoveRequest(&myrequest1)) != OSMP_SUCCESS)
+    if((status = OSMP_Irecv(irecv, 13, OSMP_typeof(*bar), &sender, &len, myrequest2)) != OSMP_SUCCESS)
         printf("Error %d\n", error);
     error++;
 
-    if ((status = OSMP_RemoveRequest(&myrequest2)) != OSMP_SUCCESS)
+    if((status = OSMP_Wait(myrequest1)) != OSMP_SUCCESS)
+        printf("Error %d\n", error);
+    error++;
+    if((status = OSMP_Wait(myrequest2)) != OSMP_SUCCESS)
         printf("Error %d\n", error);
     error++;
 
-    printf("message from nonblocking is:\t\"%s\"\n", irecv);
-    fflush(NULL);
+    if((status = OSMP_RemoveRequest(&myrequest1)) != OSMP_SUCCESS)
+        printf("Error %d\n", error);
+    error++;
+    if((status = OSMP_RemoveRequest(&myrequest2)) != OSMP_SUCCESS)
+        printf("Error %d\n", error);
+    error++;
 
-    if ((status = OSMP_Finalize() != OSMP_SUCCESS))
+    printf("message from nonblocking is:\t\"%s\"\n", irecv);fflush(NULL);
+
+    if((status = OSMP_Finalize() != OSMP_SUCCESS))
         printf("Error %d\n", error);
     error++;
     return 0;
--- osmplib/src/queue.c
+++ osmplib/src/queue.c
@@ -7,15 +7,6 @@
  * 
  * @date April 2019
  */
-#include <semaphore.h>/**
- * @file queue.c
- * 
- * @brief The osmplib header 
- * 
- * @author Daniel Knüppe, Roderick Topütt 
- * 
- * @date April 2019
- */
 #include <semaphore.h>
 #include "queue.h"
 
@@ -78,68 +69,5 @@ int destroy_queue(OSMP_queue *queue)
         return(OSMP_ERROR);
     if(sem_destroy(&queue->queue_lock))
         return(OSMP_ERROR);
-    return OSMP_SUCCESS;
-}
-
-#include "queue.h"
-
-void push(OSMP_msg_node *node, OSMP_queue *queue)
-{
-    /* lock further access to queue */
-    sem_wait(&queue->queue_lock);
-    /* make sure not to push more than the queue can (or should) handle */
-    sem_wait(&queue->max_length);
-
-    /* get index for messages[] in shm from node */
-    unsigned int index = node - base->messages;
-    /* check if queue is empty */
-    if ((queue->back == queue->front) && (queue->back == -1))
-        queue->front = queue->back = index;
-    node->next = -1;
-    base->messages[queue->back].next = index;
-    queue->back = index;
-
-    /* tell the queue that another node is available / has been pushed to it */
-    sem_post(&queue->availabe);
-    /* unlock access to the queue */
-    sem_post(&queue->queue_lock);
-}
-
-OSMP_msg_node *pop(OSMP_queue *queue)
-{
-    /* lock further access to queue */
-    sem_wait(&queue->queue_lock);
-    /* make sure the queue is not empty / wait for it to get a new node */
-    sem_wait(&queue->availabe);
-
-    unsigned int index = queue->front;
-    queue->front = base->messages[queue->front].next;
-
-    /* tell the queue that another node has been popped */
-    sem_post(&queue->max_length);
-    /* unlock access to the queue */
-    sem_post(&queue->queue_lock);
-    return &base->messages[index];
-}
-
-int init_queue(OSMP_queue *queue, unsigned int max_length)
-{
-    if (sem_init(&queue->max_length, 1, max_length))
-        return (OSMP_ERROR);
-    if (sem_init(&queue->availabe, 1, 0))
-        return (OSMP_ERROR);
-    if (sem_init(&queue->queue_lock, 1, 1))
-        return (OSMP_ERROR);
-    return OSMP_SUCCESS;
-}
-
-int destroy_queue(OSMP_queue *queue)
-{
-    if (sem_destroy(&queue->max_length))
-        return (OSMP_ERROR);
-    if (sem_destroy(&queue->availabe))
-        return (OSMP_ERROR);
-    if (sem_destroy(&queue->queue_lock))
-        return (OSMP_ERROR);
     return OSMP_SUCCESS;
 }
